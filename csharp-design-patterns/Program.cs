2bd95653add09ff2d2aebfbc0ad5020149682a29
ICreateThunder <iamrobshalr@gmail.com>
2023-11-01 10:02:59 +0000

    Add project files.

---
+++ csharp-design-patterns/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_design_patterns
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* [STRATERGY PATTERN]
+             * 
+             * Components:
+             * 
+             * - Context     : Able to select which stratergy best suits at runtime
+             * - IStratergy  : Interface provides layws of abstraction between business logic and lower level implementation
+             * - Statergy    : Lower level implementation
+             *
+             * Demonstration:
+             * 
+             * - Context     : Program class (Main Function) | 
+             * - IStratergy  : 
+             * - Stratergy   : 
+             * 
+             */
+        }
+    }
+}
